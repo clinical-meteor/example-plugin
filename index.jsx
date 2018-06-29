23d9550e162b4e184c944d34cf94858cc4c051b5
Abigail Watson <abigailwatson@Abigails-Mac-mini-317772.local>
2018-06-29 14:34:05 -0500

    BodyMassIndexPage

--- index.jsx
+++ index.jsx
@@ -1,16 +1,25 @@
 import HelloWorldPage from './client/HelloWorldPage.jsx';
 import PostcardPage from './client/PostcardPage.jsx';
+import BodyMassIndexPage from './client/BodyMassIndexPage.jsx';
 
 var DynamicRoutes = [{
   'name': 'HelloWorldPage',
   'path': '/hello-world',
   'component': HelloWorldPage
+}, {
+  'name': 'BodyMassIndexPage',
+  'path': '/body-mass-index',
+  'component': BodyMassIndexPage
 }];
 
 var SidebarElements = [{
   'primaryText': 'Example Page',
   'to': '/hello-world',
   'href': '/hello-world'
+}, {
+  'primaryText': 'Body Mass Calculator',
+  'to': '/body-mass-index',
+  'href': '/body-mass-index'
 }];
 
-export { SidebarElements, DynamicRoutes, SamplePage, PostcardPage };
+export { SidebarElements, DynamicRoutes, SamplePage, PostcardPage, BodyMassIndexPage };
